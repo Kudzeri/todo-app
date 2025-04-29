85d27750e0156e521b7c1e0e146d1894a020cc58
Kudzeri <kudzeri@gmail.com>
2025-04-29 12:01:42 +0500

    add react router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+
+fetch('http://localhost:3001/users').then(res => console.log(res))
+
 const app = () => {
   return (
-    <div>app</div>
+    <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/:userId/tasks" element={<h1>Tasks</h1>} />
+    </Routes>
   )
 }
 
